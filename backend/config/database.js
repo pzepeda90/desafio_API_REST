13e98dc63f247e8341386a9e3ba230cf9d2e3414
Patricio Zepeda <patriciozepeda@MacBook-Pro.local>
2025-04-19 23:25:59 -0400

    consolido en database.js, los 3 archivos config.js, connection.js y database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'patriciozepeda',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'joyas',
-    password: process.env.DB_PASSWORD || '123456',
-    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    allowExitOnIdle: true
 });
+
+// Verificar la conexión a la base de datos
+pool.query("SELECT NOW()")
+    .then(() => console.log("Database connected successfully"))
+    .catch(error => console.log("Error connecting to database:", error));
 
 export default pool; 
